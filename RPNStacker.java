c5ed7be73eca446c7abcc71bb8fdbeb3dc98eab6
mvbs3 <mvbs3@cin.ufpe.br>
2022-04-28 11:46:44 -0300

    Simple scanning(lexer) implemented

--- RPNStacker.java
+++ RPNStacker.java
@@ -1,35 +1,80 @@
 import java.util.Stack;
+import java.util.ArrayList;
 import java.io.*;
-
+import stacker.rpn.lexer.Token;
+import stacker.rpn.lexer.TokenType;
 
 public class RPNStacker {
-    public static void main(String[] args) {
-      try {
-      String in, out;
-      String a,b;
-      Stack<String> myStack = new Stack();
+  static ArrayList<Token> scanner() {
+    Token token = new Token(TokenType.EOF, "");
+    ArrayList<Token> tokens = new ArrayList<Token>();
+    try{
+      String inLocal;
       FileInputStream myFile = new FileInputStream("Calc1.stk");
       BufferedReader buffer = new BufferedReader(new InputStreamReader(myFile));
 
-      while((in = buffer.readLine()) != null){
+      
+      
+      while((inLocal = buffer.readLine()) != null){
+        if(inLocal.equals("+")){
+          token = new Token(TokenType.PLUS, "+");
+        } else if(inLocal.equals("-")){
+          token = new Token(TokenType.MINUS, "-");
+        }else if(inLocal.equals("*")){
+          token = new Token(TokenType.STAR, "*");
+        }else if(inLocal.equals("/")){
+          token = new Token(TokenType.SLASH, "/");
+        }else{
+          String tester = "0123456789";
+          int flag =1;
+          for (int i=0; i<inLocal.length(); i++) {
+            char c = inLocal.charAt(i);
+            if(tester.indexOf(c)==-1){
+              flag=0;
+              break;
+            }       
+          }
+          if (flag==1){
+            token = new Token(TokenType.NUM, inLocal);
+          } else{
+              System.out.println("Error: Unexpected character: " + inLocal);
+              System.exit(-1);
+          }
+        }
+        tokens.add(token);
+      }
+      buffer.close();
+    }catch(IOException except){
+      System.out.println("File was not found.");
+    }
+    return tokens;
+  }
+    public static void main(String[] args) {
+      String in, out;
+      String a,b,tokenAux;
+      ArrayList<Token> tokenList = new ArrayList<Token>();
+      Stack<String> myStack = new Stack<String>();
+      
+      tokenList = scanner();
+      for (Token count : tokenList){
+        System.out.println(count.toString());
+        in = count.lexeme;
         if(in.equals("*") || in.equals("/") || in.equals("+") || in.equals("-") || in.equals("^")){
-            b = (String) myStack.pop();
-            a = (String) myStack.pop();
+            b =  myStack.pop();
+            a =  myStack.pop();
+            //CHECAR SE O TOKEN EH VALIDO SE NAO FOR, SAI, SE FOR FAZ!
             switch (in){
               case "*":
-                  myStack.push(Double.toString(Double.parseDouble(a) * Double.parseDouble(b)));
+                  myStack.push(Integer.toString(Integer.parseInt(a) * Integer.parseInt(b)));
                   break;
               case "/":
-                  myStack.push(Double.toString(Double.parseDouble(a) / Double.parseDouble(b)));
+                  myStack.push(Integer.toString(Integer.parseInt(a) / Integer.parseInt(b)));
                   break;
               case "+":
-                  myStack.push(Double.toString(Double.parseDouble(a) + Double.parseDouble(b)));
+                  myStack.push(Integer.toString(Integer.parseInt(a) + Integer.parseInt(b)));
                   break;
               case "-":
-                  myStack.push(Double.toString(Double.parseDouble(a) - Double.parseDouble(b)));
-                  break;
-              case "^":
-                  myStack.push(Double.toString(Math.pow(Double.parseDouble(a), Double.parseDouble(b))));
+                  myStack.push(Integer.toString(Integer.parseInt(a) - Integer.parseInt(b)));
                   break;
                 }     
         }else{
@@ -40,10 +85,6 @@ public class RPNStacker {
       }
       System.out.println(myStack.pop());     
 
-      buffer.close();
-      }catch (IOException except){
-        System.out.println("File was not found.");
-      }
       
     }
   }
